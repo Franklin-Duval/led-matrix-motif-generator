81dffdb174d2ad78dbcb35ec6d2aaff4468d33a8
Talom <franklinfrost14@gmail.com>
2021-11-25 15:55:06 +0100

    feat: add routes in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { GeneratorPage } from './modules/generator';
+import { HomePage } from './modules/homePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/generator' component={GeneratorPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
