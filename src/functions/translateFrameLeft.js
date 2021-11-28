8d9034cb6e9643a7b0a17e3220dbfeaee8f420cd
Cesar MH <nohocesar@gmail.com>
2021-11-28 18:22:12 +0100

    Some update

--- src/functions/translateFrameLeft.js
+++ src/functions/translateFrameLeft.js
@@ -1,16 +1,12 @@
 import clone from './clone';
 
-const translateLeft = (display) => {
-  let res = clone(display);
-  for (let matrix of res) {
+const translateLeft = (frame) => {
+  let res = clone(frame);
+  for (let matrix in res) {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 7; j++) {
-        matrix[i][j] = matrix[i][j + 1];
+        res[matrix][i][j] = res[matrix][i][j + 1];
       }
-    }
-
-    for (let i = 0; i < 8; i++) {
-      matrix[i][7] = 0;
     }
   }
 
