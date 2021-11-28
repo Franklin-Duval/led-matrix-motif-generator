5dc90d8349646da696c197499b331c249354dfc3
Cesar MH <nohocesar@gmail.com>
2021-11-28 18:46:30 +0100

    fix: translateLeft and Right bug

--- src/functions/translateFrameLeft.js
+++ src/functions/translateFrameLeft.js
@@ -2,15 +2,20 @@ import clone from './clone';
 
 const translateLeft = (display) => {
   let res = clone(display);
-  for (let matrix of res) {
+  for (let matrix in res) {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 7; j++) {
-        matrix[i][j] = matrix[i][j + 1];
+        res[matrix][i][j] = res[matrix][i][j + 1];
       }
     }
-
-    for (let i = 0; i < 8; i++) {
-      matrix[i][7] = 0;
+    if (Number(matrix) === res.length - 1) {
+      for (let i = 0; i < 8; i++) {
+        res[matrix][i][7] = 0;
+      }
+    } else {
+      for (let i = 0; i < 8; i++) {
+        res[matrix][i][7] = res[Number(matrix) + 1][i][0];
+      }
     }
   }
 
--- src/functions/translateFrameRight.js
+++ src/functions/translateFrameRight.js
@@ -1,20 +1,24 @@
 import clone from './clone';
 
-const translateRight = (frame) => {
-  let res = clone(frame);
-  for (let matrix of res) {
+const translateLeft = (display) => {
+  let res = clone(display);
+  for (let matrix = res.length - 1; matrix >= 0; matrix--) {
     for (let i = 0; i < 8; i++) {
-      for (let j = 7; j >= 1; j--) {
-        matrix[i][j] = matrix[i][j - 1];
+      for (let j = 7; j >= 0; j--) {
+        if (j >= 1) {
+          res[matrix][i][j] = res[matrix][i][j - 1];
+        } else {
+          if (matrix !== 0) {
+            res[matrix][i][0] = res[matrix - 1][i][7];
+          } else {
+            res[matrix][i][0] = 0;
+          }
+        }
       }
-    }
-
-    for (let i = 0; i < 8; i++) {
-      matrix[i][0] = 0;
     }
   }
 
   return res;
 };
 
-export default translateRight;
+export default translateLeft;
